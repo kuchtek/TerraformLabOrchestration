167ee6d6726cfdc541775067de4275f291e80fdb
kuchtek <marcin@kuchczynscy.pl>
2023-05-19 22:14:45 +0200

    Init pipeline

---
+++ terraform_code/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "v1.4.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-markuc"
+    key    = "network/terraform.tfstate"
+    region = var.region
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
