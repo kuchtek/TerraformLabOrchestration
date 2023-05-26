1c8106318daf9e39d4c1b5d54c8982c55e5a2a1c
kuchtek <marcin@kuchczynscy.pl>
2023-05-26 14:54:05 +0200

    Adding missing var

--- terraform_code/ec2.tf
+++ terraform_code/ec2.tf
@@ -13,11 +13,12 @@ resource "aws_instance" "ansible_test" {
 
 resource "aws_instance" "ansible_test_ubuntu" {
   ami                    = data.aws_ami.amznubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["${aws_security_group.ssh_in.id}"]
   key_name               = "lab-key"
   tags = {
     "Name"    = "ansible_test_ubuntu"
     "purpose" = "lab"
   }
+  
 }
