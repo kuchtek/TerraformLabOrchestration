67af266caaf025d55c0ddde22cd6cb5badbed44a
kuchtek <marcin@kuchczynscy.pl>
2023-05-26 14:57:11 +0200

    Updating an instance type

--- terraform_code/ec2.tf
+++ terraform_code/ec2.tf
@@ -2,7 +2,7 @@
 
 resource "aws_instance" "ansible_test" {
   ami                    = data.aws_ami.amznlinux.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["${aws_security_group.ssh_in.id}"]
   key_name               = "lab-key"
   tags = {
