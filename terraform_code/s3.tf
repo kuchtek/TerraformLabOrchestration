6e227f8d330d15bfd05ca0db804f45437d44d045
kuchtek <marcin@kuchczynscy.pl>
2023-05-26 13:40:48 +0200

    initializing a ec2 lab scenario

--- terraform_code/s3.tf
+++ terraform_code/s3.tf
@@ -1,20 +1,4 @@
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www-markuc-bucket"
   /* acl = "public-read" */
-  policy = <<EOF
-{
-     "id" : "MakePublic",
-   "version" : "2012-10-17",
-   "statement" : [
-      {
-         "action" : [
-             "s3:GetObject"
-          ],
-         "effect" : "Allow",
-         "resource" : "arn:aws:s3:::www-markuc-bucket/*",
-         "principal" : "*"
-      }
-    ]
-  }
-EOF
 }
